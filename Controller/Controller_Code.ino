36a790c1fa71e19737644fe4365d542789f17c70
judson-r <139906011+judson-r@users.noreply.github.com>
2023-08-12 18:49:10 -0500

    Add files via upload

--- Controller/Controller_Code.ino
+++ Controller/Controller_Code.ino
@@ -1,4 +1,4 @@
-#include <RF24G.h>
+#include <rf24g.h>
 
 //Pins
   //Analog
@@ -21,16 +21,39 @@
       int MIpin = 12;
       int SCKpin = 13;
 
-
+RF24_G send;
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
 
   analogReference(DEFAULT);
+  send = RF24_G(4, 7, 8);
 }
 
 
 void loop() {
   // put your main code here, to run repeatedly:
+   uint8_t randNumber = random(300);
+  
+  int actual;
+  
+  packet senderPacketVar;
+
+  packet receiverPacketVar;
+  // set the destination of the packet to address 1
+  sender.setAddress(1);
+  // write the payload to the packet
+  sender.addPayload(&randNumber, sizeof(int));
+  // print out the original payload
+  Serial.print("original number:");
+  Serial.println(randNumber);
+  // send the packet, if it is successful try to read back the packet
+  if (send.write(&sender) == true) {
+    // wait until a packet is received
+    while (send.available() != true);
+    
+    send.read(&receiver);
+    
+    Serial.println(actual);
   
 }
