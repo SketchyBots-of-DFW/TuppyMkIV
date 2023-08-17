cd29d67b9a94b9f7d8e7840a29c623a9136feb80
judson-r <139906011+judson-r@users.noreply.github.com>
2023-08-17 16:14:33 -0500

    Add files via upload

--- Boat/Arduino/Boat_Code.ino
+++ Boat/Arduino/Boat_Code.ino
@@ -1,5 +1,21 @@
-#include <rf24g.h>
+#include "SPI.h"
+#include "NRFLite.h"
 #include <Servo.h>
+
+//begin Radio Setup
+const static uint8_t RADIO_ID = 1;       // Our radio's id.  The transmitter will send to this id.
+
+
+struct RadioPacket // Any packet up to 32 bytes can be sent.
+{
+    uint8_t FromRadioId;
+    uint32_t OnTimeMillis;
+    uint32_t FailedTxCount;
+};
+
+NRFLite _radio;
+RadioPacket _radioData;
+//End radio Setup
 
 //Pins
   //Analog
@@ -28,30 +44,46 @@
 
 
     //Transceiver
-      int CEpin = 7;
-      int CSNpin = 8;
-      int MOpin = 11;
-      int MIpin = 12;
-      int SCKpin = 13;
+      int CEpin = 49;
+      int CSNpin = 53;
+      int MOpin = 50;
+      int MIpin = 51;
+      int SCKpin = 52;
 
 Servo pololu;
 
 Servo leftMotor;
 Servo rightMotor;
 
-double packetIn[] = {0, 0, 0, 0, 0, 0, 0};
-  int key = 0;
-  int modeSwitch = 1;
-  int launchMissiles = 2;
-  int leftJoy_Y = 3;
-  int rightJoy_Y = 4;
-  int leftJoy_X = 5;
-  int rightJoy_X = 6;
+//FIXME ////////////////////////////////////////////////////////////
+int packetIn[] = {0,0,0,0,0,0,0,0,0,0,};
+int key = 0;
+int modeSwitch = 1;
+int leftJoy_Y = 2;
+int leftJoy_X = 3;
+int rightJoy_Y = 4;
+int rightJoy_X = 5;
+//FIXME /////////////////////////////////////////////////////////////
+
 
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(9600);
+  Serial.begin(115200);
+
+    // By default, 'init' configures the radio to use a 2MBPS bitrate on channel 100 (channels 0-125 are valid).
+    // Both the RX and TX radios must have the same bitrate and channel to communicate with each other.
+    // You can run the 'ChannelScanner' example to help select the best channel for your environment.
+    // You can assign a different bitrate and channel as shown below.
+    //   _radio.init(RADIO_ID, PIN_RADIO_CE, PIN_RADIO_CSN, NRFLite::BITRATE2MBPS, 100) // THE DEFAULT
+    //   _radio.init(RADIO_ID, PIN_RADIO_CE, PIN_RADIO_CSN, NRFLite::BITRATE1MBPS, 75)
+    //   _radio.init(RADIO_ID, PIN_RADIO_CE, PIN_RADIO_CSN, NRFLite::BITRATE250KBPS, 0)
+    
+    if (!_radio.init(RADIO_ID, CEpin, CSNpin))
+    {
+        Serial.println("Cannot communicate with radio");
+        while (1); // Wait here forever.
+    }
 
   analogReference(DEFAULT);
 
@@ -110,27 +142,44 @@ void tankSteering(){
 
 void loop() {
   // put your main code here, to run repeatedly:
+  while (_radio.hasData())
+    {
+        _radio.readData(&_radioData); // Note how '&' must be placed in front of the variable name.
+
+        String msg = "Radio ";
+        msg += _radioData.FromRadioId;
+        msg += ", ";
+        msg += _radioData.OnTimeMillis;
+        msg += " ms, ";
+        msg += _radioData.FailedTxCount;
+        msg += " Failed TX";
+
+        Serial.println(msg);
+
+        if(isManual()){
+
+          activatePololu();
+
+          tankSteering();
+
+        }
+        else if(isAutonomous()){
+
+          activatePololu();
+
+          //FIXME/////////////////////////////////////////////
+          setPercent(leftMotor, 0.5);
+          setPercent(rightMotor, -0.5);
+          ////////////////////////////////////////////////////
+
+        }
+        else{
+
+          deactivatePololu();
+
+        }
+      }
   
-  if(isManual()){
-
-    activatePololu();
-
-    tankSteering();
-
-  }
-  else if(isAutonomous()){
-
-    activatePololu();
-
-    //FIXME/////////////////////////////////////////////
-    setPercent(leftMotor, 0.5);
-    setPercent(rightMotor, -0.5);
-    ////////////////////////////////////////////////////
-
-  }
-  else{
-
-    deactivatePololu();
-
-  }
+  
+  
 }
