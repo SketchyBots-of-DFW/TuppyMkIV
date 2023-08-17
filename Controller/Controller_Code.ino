78273f8579c55aa2772d9d909b72918e64bd7125
judson-r <139906011+judson-r@users.noreply.github.com>
2023-08-17 16:09:53 -0500

    Changed Radio library

--- Controller/Controller_Code.ino
+++ Controller/Controller_Code.ino
@@ -1,4 +1,20 @@
-#include <rf24g.h>
+#include "SPI.h"
+#include "NRFLite.h"
+
+//Radio Setup Begin
+const static uint8_t RADIO_ID = 2;             // Controller radio's id.
+const static uint8_t DESTINATION_RADIO_ID = 1; // Id of the boat radio.
+
+struct RadioPacket // Any packet up to 32 bytes can be sent.
+{
+    uint8_t FromRadioId;
+    uint32_t OnTimeMillis;
+    uint32_t FailedTxCount;
+};
+
+NRFLite _radio;
+RadioPacket _radioData;
+//Radio Setup End
 
 //Pins
   //Analog
@@ -14,49 +30,60 @@
 
 
 
-    //Transceiver
-      int CEpin = 7;
-      int CSNpin = 8;
-      int MOpin = 11;
-      int MIpin = 12;
-      int SCKpin = 13;
+    //Transmitter
+    uint8_t CEpin = 9;
+    uint8_t CSNpin = 10;
+    int MOpin = 11;
+    int MIpin = 12;
+    int SCKpin = 12;
 
-RF24_G send;
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(9600);
-
-  analogReference(DEFAULT);
-  send = RF24_G(4, 7, 8);
+  Serial.begin(115200);
+    
+    // By default, 'init' configures the radio to use a 2MBPS bitrate on channel 100 (channels 0-125 are valid).
+    // Both the RX and TX radios must have the same bitrate and channel to communicate with each other.
+    // You can run the 'ChannelScanner' example to help select the best channel for your environment.
+    // You can assign a different bitrate and channel as shown below.
+    //   _radio.init(RADIO_ID, PIN_RADIO_CE, PIN_RADIO_CSN, NRFLite::BITRATE2MBPS, 100) // THE DEFAULT
+    //   _radio.init(RADIO_ID, PIN_RADIO_CE, PIN_RADIO_CSN, NRFLite::BITRATE1MBPS, 75)
+    //   _radio.init(RADIO_ID, PIN_RADIO_CE, PIN_RADIO_CSN, NRFLite::BITRATE250KBPS, 0)
+    
+    if (!_radio.init(RADIO_ID, CEpin, CSNpin))
+    {
+        Serial.println("Cannot communicate with radio");
+        while (1); // Wait here forever.
+    }
+    
+    _radioData.FromRadioId = RADIO_ID;
 }
 
 
 void loop() {
   // put your main code here, to run repeatedly:
-  uint8_t sendable = 2040;
-  
-  int actual;
-  
-  packet senderPacketVar;
+  _radioData.OnTimeMillis = millis();
 
-  packet receiverPacketVar;
-  // set the destination of the packet to address 1
-  senderPacketVar.setAddress(1);
-  // write the payload to the packet
-  senderPacketVar.addPayload(&sendable, sizeof(int));
-  // print out the original payload
-  Serial.print("original number:");
-  Serial.println(sendable);
-  // send the packet, if it is successful try to read back the packet
-  if (send.write(&senderPacketVar) == true) {
-    // wait until a packet is received
-    while (send.available() != true);
-    receiverPacketVar.readPayload(&actual, sizeof(int));
-    // print out the actual value received
-    Serial.print("received number:");
-    send.read(&receiverPacketVar);
+    Serial.print("Sending ");
+    Serial.print(_radioData.OnTimeMillis);
+    Serial.print(" ms");
+
+    // By default, 'send' transmits data and waits for an acknowledgement.  If no acknowledgement is received,
+    // it will try again up to 16 times.  This retry logic is built into the radio hardware itself, so it is very fast.
+    // You can also perform a NO_ACK send that does not request an acknowledgement.  In this situation, the data packet
+    // will only be transmitted a single time and there is no verification of delivery.  So NO_ACK sends are suited for
+    // situations where performance is more important than reliability.
+    //   _radio.send(DESTINATION_RADIO_ID, &_radioData, sizeof(_radioData), NRFLite::REQUIRE_ACK) // THE DEFAULT
+    //   _radio.send(DESTINATION_RADIO_ID, &_radioData, sizeof(_radioData), NRFLite::NO_ACK)
     
-    Serial.println(actual);
-  
-  }
+    if (_radio.send(DESTINATION_RADIO_ID, &_radioData, sizeof(_radioData))) // Note how '&' must be placed in front of the variable name.
+    {
+        Serial.println("...Success");
+    }
+    else
+    {
+        Serial.println("...Failed");
+        _radioData.FailedTxCount++;
+    }
+
+    delay(1000);
 }
