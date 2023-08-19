d2719a575ca5a4cf65291f36821af656a0328e77
judson-r <139906011+judson-r@users.noreply.github.com>
2023-08-19 17:48:07 -0500

    Add files via upload

---
+++ GroundStation/GroundStationArduino/GroundStationArduino.ino
@@ -0,0 +1,45 @@
+//Pins
+  //Analog
+    //In
+      int joystickXpin = A0;
+      int joystickYpin = A1;
+      int twist = A2;
+      int thrust = A3;
+
+    //Out
+      int lcdSDA = A4;
+      int lcdSCL = A5;
+
+  //Digital
+    //In
+      int keyPin = 2;
+      int autonSwitchPin = 3;
+
+
+    //Out
+      
+
+
+
+    //Transmitter
+      uint8_t CEpin = 9;
+      uint8_t CSNpin = 10;
+      int MOpin = 11;
+      int MIpin = 12;
+      int SCKpin = 12;
+
+    //Receiver
+      int outD0 = 4;
+      int outD1 = 5;
+      int outD2 = 6;
+      int outD3 = 7;
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
